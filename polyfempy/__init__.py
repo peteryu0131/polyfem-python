3122aa48a872368894dfa907b23050d220f15ca3
Huangzizhou <zizhou@nyu.edu>
2024-10-10 17:24:53 -0400

    comment missing files

--- polyfempy/__init__.py
+++ polyfempy/__init__.py
@@ -1,15 +1,15 @@
 from .polyfempy import *
 
-from .Settings import Settings
-from .Selection import Selection
+# from .Settings import Settings
+# from .Selection import Selection
 
-from .Problem import Problem
+# from .Problem import Problem
 
-from .Problems import Franke
-from .Problems import GenericScalar
-from .Problems import Gravity
-from .Problems import Torsion
-from .Problems import GenericTensor
-from .Problems import Flow
-from .Problems import DrivenCavity
-from .Problems import FlowWithObstacle
+# from .Problems import Franke
+# from .Problems import GenericScalar
+# from .Problems import Gravity
+# from .Problems import Torsion
+# from .Problems import GenericTensor
+# from .Problems import Flow
+# from .Problems import DrivenCavity
+# from .Problems import FlowWithObstacle
